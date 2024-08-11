2e327e9da5ffb68c8799a508499ca5364250c8ea
MLankaoui <marouanelankaoui66@gmail.com>
2024-08-11 15:00:03 +0100

    adding more testing in main.go

--- main.go
+++ main.go
@@ -15,5 +15,9 @@ func main() {
 	fmt.Println(utils.HeaderParser("##### WELCOME TO OUR PAGE"))
 	fmt.Println(utils.HeaderParser("###### WELCOME TO OUR PAGE"))
 
-	// fmt.Print
+	// test decoration
+	fmt.Println(utils.Decoration("**bold**"))
+	fmt.Println(utils.Decoration("*italic*"))
+	fmt.Println(utils.Decoration("word"))
+	fmt.Println(utils.Decoration("~~strikethrough~~"))
 }
